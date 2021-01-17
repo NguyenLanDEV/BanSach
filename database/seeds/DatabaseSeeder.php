4e112c4a1ca198dfb54f75a554a55a63c27aa6ae
lanHALOVI <nguyenthelandev@gmail.com>
2021-01-17 20:28:13 +0700

    updated seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -83,19 +83,73 @@ class DatabaseSeeder extends Seeder
         ]);
 
         DB::table('nhomSP')->insert([
+        [
             "tenNSP" => "Thiếu Nhi",
             "maDM" => 1,
-        ], [
+        ],
+        [
             "tenNSP" => "Kỹ năng sống",
             "maDM" => 1,
-        ], [
+        ],
+        [
             "tenNSP" => "Văn học",
             "maDM" => 1,
-        ], [
+        ],
+        [
             "tenNSP" => "Kinh tế",
             "maDM" => 1,
+        ]
         ]);
-
+        DB::table('HinhAnh')->insert([
+            [
+                'maSP' => 1,
+                'duongDan' => 'images/product/sach/dac-nhan-tam.jpg'
+            ],
+            [
+                'maSP' => 2,
+                'duongDan' => 'images/product/sach/e167641dc7cd88d776b8205d9bd65b20.jpg'
+            ],
+            [
+                'maSP' => 3,
+                'duongDan' => 'images/product/sach/17262447faaef713be60d6b25ec0551d.jpg'
+            ],
+            [
+                'maSP' => 4,
+                'duongDan' => 'images/product/sach/cac2e0ac6f4395d400d29f1aba941d68.jpg'
+            ],
+            [
+                'maSP' => 5,
+                'duongDan' => 'images/product/sach/202015881b04b7c8a4b47d15cf9c049f.jpg'
+            ],
+            [
+                'maSP' => 6,
+                'duongDan' => 'images/product/sach/537f7b9bb219be81e217e0c7b5b037e6.jpg'
+            ],
+            [
+                'maSP' => 7,
+                'duongDan' => 'images/product/sach/c4a78eb2f28d4af61b3d669ee4b2bfb6.jpg'
+            ],
+            [
+                'maSP' => 8,
+                'duongDan' => 'images/product/sach/6e0910413382398e83754498dfbf725d.jpg'
+            ],
+            [
+                'maSP' => 9,
+                'duongDan' => 'images/product/sach/0dcc6e04aca0a78cda09a2d8b156dccb.jpg'
+            ],
+            [
+                'maSP' => 10,
+                'duongDan' => 'images/product/sach/902927dd448914d027d63e8c50a2cf28.jpg'
+            ],
+            [
+                'maSP' => 11,
+                'duongDan' => 'images/product/sach/36e9a78448c9cb46d94f49e0e507b569.jpg'
+            ],
+            [
+                'maSP' => 12,
+                'duongDan' => 'images/product/sach/b1315d221851b6ae34d06069bc4f1dce.jpg'
+            ]
+        ]);
         DB::table('SanPham')->insert([
             [
                 'tenSP' => "Đắc nhân tâm",
@@ -187,7 +241,7 @@ class DatabaseSeeder extends Seeder
                 'updated_at' => new DateTime(),
             ],
             [
-                'tenSP' => "Con Chim Xanh Biếc Bay Về ",
+                'tenSP' => "Biến Thân",
                 'gia' => rand(100000, 1000000),
                 'soLuong' => rand(1000, 100000),
                 'maTG' => 1,
